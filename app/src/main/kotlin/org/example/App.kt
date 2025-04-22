a7dad7d1075904cd40d02cbf3d3e234ab733c7e7
Gaspar Gimenez <gm.gimenez@alumno.etec.um.edu.ar>
2025-04-22 12:55:31 -0300

    paso 2

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -3,17 +3,30 @@ package org.example
 class App {
     val greeting: String
         get() {
-            return "Paso 1!"
+            return "Paso 2!"
         }
 }
 fun main() {
     println("¡Hola, Kotlin!")
-    imprimirNombreYFecha()
 }
 
-fun imprimirNombreYFecha() {
-    val nombre = "Juan"
-    val fechaActual = "22 de abril"  
-    println("Mi nombre es: $nombre")
-    println("La fecha actual es: $fechaActual")
+fun mostrarTiposDeDatos() {
+    val nombre: String = "Gaspar Giemenez"
+    val edad: Int = 16
+    val altura: Double = 1.80
+    val esEstudiante: Boolean = true
+
+    println("Nombre: $nombre")
+    println("Edad: $edad")
+    println("Altura: $altura")
+    println("Es estudiante: $esEstudiante")
+    mostrarTiposDeDatos()
+}
+
+fun calcularYMostrarAreaRectangulo() {
+    val base = 5
+    val altura = 3
+    val area = base * altura
+    println("El área del rectángulo con base $base y altura $altura es: $area")
+    calcularYMostrarAreaRectangulo()
 }
