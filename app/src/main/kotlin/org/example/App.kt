c799a47b00d61e0ccd4bb7bfb5155ba9824b339b
Gaspar Gimenez <gm.gimenez@alumno.etec.um.edu.ar>
2025-04-25 10:07:32 -0300

    paso 3

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -8,6 +8,9 @@ class App {
 }
 fun main() {
     println("¡Hola, Kotlin!")
+    mostrarTiposDeDatos()
+    calcularYMostrarAreaRectangulo()
+    printeos()
 }
 
 fun mostrarTiposDeDatos() {
@@ -20,7 +23,6 @@ fun mostrarTiposDeDatos() {
     println("Edad: $edad")
     println("Altura: $altura")
     println("Es estudiante: $esEstudiante")
-    mostrarTiposDeDatos()
 }
 
 fun calcularYMostrarAreaRectangulo() {
@@ -28,5 +30,37 @@ fun calcularYMostrarAreaRectangulo() {
     val altura = 3
     val area = base * altura
     println("El área del rectángulo con base $base y altura $altura es: $area")
-    calcularYMostrarAreaRectangulo()
 }
+
+//paso 3
+
+fun esParOImpar(numero: Int): String {
+    return if (numero % 2 == 0) {
+        "Par"
+    } else {
+        "Impar"
+    }
+}
+fun determinarSigno(numero: Int): String {
+    return when {
+        numero > 0 -> "Positivo"
+        numero < 0 -> "Negativo"
+        else -> "Cero"
+    }
+}
+fun printeos() {
+    val numero1: Int = 4
+    val numero2: Int = -3
+
+    println("El número $numero1 es: ${esParOImpar(numero1)}")
+    println("El número $numero2 es: ${esParOImpar(numero2)}")
+
+    println("El número $numero1 es: ${determinarSigno(numero1)}")
+    println("El número $numero2 es: ${determinarSigno(numero2)}")
+}
+
+//paso 4
+
+
+
+
